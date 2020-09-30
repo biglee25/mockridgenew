9e96e70c7b2fb638a211eed7a758f017db4fd6da
Lee Attwater <leeattwater@gmail.com>
2020-09-30 17:46:53 +0100

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,7 +36,6 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </div>
 );
 
