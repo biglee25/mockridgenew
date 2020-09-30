0adbac86939b4a7e332fffe42995f296fbae2d50
Lee Attwater <leeattwater@gmail.com>
2020-09-30 17:07:25 +0100

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react";
 import { Link } from "gatsby";
 
-import Layout from "../components/layout";
 import Image from "../components/image";
-import SEO from "../components/seo";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: black;
+  color: white;
+  padding: 2rem 1rem;
+
+  h1 {
+    font-family: Anton;
+    font-size: 5rem;
+  }
+`;
+
+const Text = styled.div`
+  p {
+    font-family: Roboto;
+    font-size: 1rem;
+    color: red;
+  }
+`;
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site</p>
+  <div>
+    <Container>
+      <h1>Menu</h1>
+    </Container>
+    <Text>
+      <p>Welcome to your new Gatsby site.</p>
+    </Text>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
 );
 
 export default IndexPage;
